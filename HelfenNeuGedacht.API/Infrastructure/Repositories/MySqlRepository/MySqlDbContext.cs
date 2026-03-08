fb1ea331186eef43b3d466826c5f34ff9ca86954
dsc3989 <dsc3989@students.fhv.at>
2026-03-08 10:19:50 +0100

    add DbSet for Shift

--- HelfenNeuGedacht.API/Infrastructure/Repositories/MySqlRepository/MySqlDbContext.cs
+++ HelfenNeuGedacht.API/Infrastructure/Repositories/MySqlRepository/MySqlDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HelfenNeuGedacht.API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using static Google.Protobuf.Compiler.CodeGeneratorResponse.Types;
 
 namespace HelfenNeuGedacht.API.Infrastructure.Repositories.MySqlRepository
@@ -6,7 +7,7 @@ namespace HelfenNeuGedacht.API.Infrastructure.Repositories.MySqlRepository
     public class MySqlDbContext : DbContext
     {
       
-
+        public DbSet<Shift> Shifts { get; set; }
 
         public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options) { }
     }

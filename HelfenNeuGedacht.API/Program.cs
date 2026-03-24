bc2b7d78e1b226d41e35cd488c35ca911d3087db
Lukas Giselbrecht <l.giselbrecht@portalum.eu>
2026-03-24 23:37:45 +0100

    Cleanup Prgram.cs

--- HelfenNeuGedacht.API/Program.cs
+++ HelfenNeuGedacht.API/Program.cs
@@ -110,15 +110,7 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.MapOpenApi();
-//    app.MapScalarApiReference(options =>
-//    {
-//        options.Title = "HelfenNeuGedacht API";
-//    });
-//}
+
 app.MapOpenApi();
 app.MapScalarApiReference(options =>
 {
